c8058ba541c96068d5051509b9b858aadd821647
Eduardo_LopesDoAmara <eduardo.lopesdoamaral@dell.com>
2025-06-10 19:05:43 -0300

    Introduce docker-compose.yaml for containerized postgreSQL

--- src/main/java/eduardoamaral/devicechallenge/components/device/DeviceController.java
+++ src/main/java/eduardoamaral/devicechallenge/components/device/DeviceController.java
@@ -1,7 +1,34 @@
 package eduardoamaral.devicechallenge.components.device;
 
-import org.springframework.web.bind.annotation.RestController;
+import eduardoamaral.devicechallenge.components.device.dto.DeviceRequestDTO;
+import eduardoamaral.devicechallenge.components.device.dto.DeviceResponseDTO;
+import eduardoamaral.devicechallenge.components.device.service.DeviceService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.http.ResponseEntity.ok;
 
 @RestController
+@RequestMapping("/devices")
+@RequiredArgsConstructor
 public class DeviceController {
+    private final DeviceService deviceService;
+
+    @PostMapping("/add")
+    public ResponseEntity<DeviceResponseDTO> addDevice(@Valid @RequestBody DeviceRequestDTO request){
+        return ok(deviceService.create(request));
+    }
+
+//    @GetMapping("/search")
+//    public ResponseEntity<DeviceResponseDTO> search(
+//            @RequestParam String model,
+//            @RequestParam String valid,
+//            @RequestParam String name
+//            ){
+//        return null;
+//    }
+
+
 }
